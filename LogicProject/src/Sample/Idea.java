fbd8b2fe503a90e3bb2af42ab30eb8bebb8f4c16
Brennan Mulligan <bcmulligan7@gmail.com>
2018-10-25 14:28:15 -0400

    yeet

--- LogicProject/src/Sample/Idea.java
+++ LogicProject/src/Sample/Idea.java
@@ -1,7 +1,51 @@
 package Sample;
 
 public class Idea {
-    int numVariables = 0;
+    int numAttributes = 0;
     boolean teacherApproval = false;
-    
+    boolean containsObjects = false;
+    boolean isItCool = false;
+
+    public static set(int a, boolean t, boolean c, boolean i)
+    {
+        numAttributes = a;
+        teacherApproval = t;
+        containsObjects = c;
+        isItCool = i;
+    }
+
+    public static int setAttributes (int a)
+    {
+        numAttributes = a;
+    }
+    public static boolean setApproval (boolean t)
+    {
+        teacherApproval = t;
+    }
+    public static boolean setObjects (boolean c)
+    {
+        containsObjects = c;
+    }
+    public static boolean setCoolness (boolean i)
+    {
+        isItCool = i;
+    }
+
+    public static int getAttributes ()
+    {
+        return numAttributes;
+    }
+    public static boolean getApproval ()
+    {
+        return teacherApproval;
+    }
+    public static boolean getObjects ()
+    {
+        return containObjects;
+    }
+    public static boolean getCoolness ()
+    {
+        return isItCool;
+    }
+
 }
